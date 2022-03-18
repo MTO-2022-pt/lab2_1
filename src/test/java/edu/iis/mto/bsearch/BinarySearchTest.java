370bfcb1931b86fae6a6c4e2a47bc2af65a4f26a
Laciatek1998 <58668409+Laciatek1998@users.noreply.github.com>
2022-03-18 18:27:27 +0100

    testy_v1 searchForElementAtFirstPositionInSeqence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
     private static final int[] ONE_ELEMENT_SEQUENCE = {2};
+    private static final int[] MULTI_ELEMENT_SEQUENCE = {-3, 0, 1, 3, 6, 9, 12};
 
     @BeforeEach
     void setUp() throws Exception {}
@@ -28,6 +29,16 @@ class BinarySearchTest {
         SearchResult search = BinarySearch.search(key, ONE_ELEMENT_SEQUENCE);
         assertFalse(search.isFound());
         assertEquals(expected_pos, search.getPosition());
+    }
+
+    @Test
+    public void searchForElementAtFirstPositionInSequence() {
+        int key = -3;
+        int expected_pos = 1;
+        SearchResult search = BinarySearch.search(key, MULTI_ELEMENT_SEQUENCE);
+        assertTrue(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+        assertEquals(MULTI_ELEMENT_SEQUENCE[search.getPosition() - 1], key);
     }
 
     @Test
