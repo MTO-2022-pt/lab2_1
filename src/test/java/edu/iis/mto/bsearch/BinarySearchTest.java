e341df7e1ed1229b2cacda6a5b062739321948bc
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-03-18 17:58:52 +0100

    testy_v1 created tests - some failed

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -2,18 +2,51 @@ package edu.iis.mto.bsearch;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 class BinarySearchTest {
-
-    @BeforeEach
-    void setUp() throws Exception {}
+    public final static int[] sequenceLong = {1, 4, 6, 8, 10, 45};
+    public final static int[] sequenceShort = { 6 };
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void inShortSequence() {
+        SearchResult result = BinarySearch.search(sequenceShort[0], sequenceShort);
+        assertTrue(result.isFound());
+        assertEquals(0, result.getPosition());
+    }
+
+    @Test
+    void notInShortSequence() {
+        assertFalse(BinarySearch.search(sequenceShort[0]+1, sequenceShort).isFound());
+    }
+
+    @Test
+    void firstInSequence() {
+        SearchResult result = BinarySearch.search(sequenceLong[1], sequenceLong);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 0);
+    }
+
+    @Test
+    void lastInSequence() {
+        SearchResult result = BinarySearch.search(sequenceLong[sequenceLong.length-1], sequenceLong);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), sequenceLong.length-1);
+    }
+
+    @Test
+    void midInSequence() {
+        SearchResult result = BinarySearch.search(sequenceLong[sequenceLong.length/2], sequenceLong);
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), sequenceLong.length/2);
+    }
+
+    @Test
+    void notInLongSequence() {
+        assertFalse(BinarySearch.search(sequenceLong[0]-1, sequenceLong).isFound());
     }
 
 }
