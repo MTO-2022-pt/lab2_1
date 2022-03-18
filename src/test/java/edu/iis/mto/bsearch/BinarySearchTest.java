feec8d8468f72224ee105e37b0b59c4e08f134d7
Laciatek1998 <58668409+Laciatek1998@users.noreply.github.com>
2022-03-18 18:00:14 +0100

    testy_v1 searchForElementInSequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -7,9 +7,19 @@ import org.junit.jupiter.api.Test;
 
 
 class BinarySearchTest {
+    private static final int[] ONE_ELEMENT_SEQUENCE = {2};
 
     @BeforeEach
     void setUp() throws Exception {}
+
+    @Test
+    public void searchForElementInSeqence(){
+        int key = 2;
+        int expected_pos = 1;
+        SearchResult search = BinarySearch.search(key, ONE_ELEMENT_SEQUENCE);
+        assertTrue(search.isFound());
+        assertEquals(expected_pos, search.getPosition());
+    }
 
     @Test
     void test() {
