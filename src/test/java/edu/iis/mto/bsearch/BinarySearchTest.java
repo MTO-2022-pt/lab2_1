4fe04e79ee8bb0c9ae8386543f40455fbd1c5648
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-03-18 18:28:41 +0100

    testy_v1 - Założenia z tabeli, poprawa kodu

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,11 +9,56 @@ import org.junit.jupiter.api.Test;
 class BinarySearchTest {
 
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void thereIsOneInSingleElementSequenceTest(){
+        int[] sequence = {1};
+        SearchResult result = BinarySearch.search(1, sequence);
+        assertTrue(result.isFound());
+        assertEquals(0, result.getPosition());
+    }
+
+    @Test
+    void thereIsNoInSingleElementSequenceTest() {
+        int[] sequence = {1};
+        SearchResult result = BinarySearch.search(2, sequence);
+        assertFalse(result.isFound());
+        assertEquals(-1, result.getPosition());
+    }
+
+    @Test
+    void firstOneInMultipleElementSequenceTest(){
+        int[] sequence = {1, 2, 3};
+        SearchResult result = BinarySearch.search(1, sequence);
+        assertTrue(result.isFound());
+        assertEquals(0, result.getPosition());
+    }
+
+
+    @Test
+    void lastOneInMultipleElementSequenceTest(){
+        int[] sequence = {1, 2, 3};
+        SearchResult result = BinarySearch.search(3, sequence);
+        assertTrue(result.isFound());
+        assertEquals(sequence.length - 1, result.getPosition());
+    }
+
+    @Test
+    void middleOneInMultipleElementSequenceTest(){
+        int[] sequence = {1, 2, 3};
+        SearchResult result = BinarySearch.search(2, sequence);
+        assertTrue(result.isFound());
+        assertEquals(1, result.getPosition());
+    }
+
+    @Test
+    void itIsNotInMultipleElementSequenceTest(){
+        int[] sequence = {1, 2, 3};
+        SearchResult result = BinarySearch.search(4, sequence);
+        assertFalse(result.isFound());
+        assertEquals(-1, result.getPosition());
     }
 
 }
