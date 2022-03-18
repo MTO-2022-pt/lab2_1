10ed3457adeb3b4ee95c32245d6af0479ab4771f
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-03-18 19:32:11 +0100

    testy_v3 - optymalizacja importów, sprawdzenie poprawności testów

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,9 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
-
 class BinarySearchTest {
 
     @BeforeEach
