9a81e1dc76b8711fe67ee56a9efc5ea9e597c1c0
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-03-18 19:18:18 +0100

    testy_v1 dodano testy jednostkowe dla SearchResult

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,12 +8,82 @@ import org.junit.jupiter.api.Test;
 
 class BinarySearchTest {
 
+    SearchResult searchResult;
+    int key = 0, keyPosition = 0;
+    int[] seq;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void shouldFindElementInOneElementSeq() {
+        seq = new int[]{key};
+        searchResult = BinarySearch.search(key, seq);
+
+        assertEquals(true, searchResult.isFound());
+        assertEquals(keyPosition, searchResult.getPosition());
+    }
+
+    @Test
+    void shouldnotFindElementInOneElementSeq() {
+        keyPosition = -1;
+        seq = new int[]{1};
+        searchResult = BinarySearch.search(key, seq);
+
+        assertEquals(false, searchResult.isFound());
+        assertEquals(keyPosition, searchResult.getPosition());
+    }
+
+    @Test
+    void isFirstElement() {
+        seq = new int[]{key, 1, 2};
+        searchResult = BinarySearch.search(key, seq);
+
+        assertEquals(true, searchResult.isFound());
+        assertEquals(keyPosition, searchResult.getPosition());
+    }
+
+    @Test
+    void isLastElement() {
+        key = 3;
+        keyPosition = 2;
+        seq = new int[]{1, 2, key};
+        searchResult = BinarySearch.search(key, seq);
+
+        assertEquals(true, searchResult.isFound());
+        assertEquals(keyPosition, searchResult.getPosition());
+    }
+
+    @Test
+    void shouldNotFind() {
+        keyPosition = -1;
+        seq = new int[]{1, 2, 3};
+        searchResult = BinarySearch.search(key, seq);
+
+        assertEquals(false, searchResult.isFound());
+        assertEquals(keyPosition, searchResult.getPosition());
+    }
+
+    @Test
+    void isMiddleElementInOddArray() {
+        seq = new int[]{-1, key, 1};
+        
+        keyPosition = seq.length / 2;
+        searchResult = BinarySearch.search(key, seq);
+        assertEquals(true, searchResult.isFound());
+        assertEquals(keyPosition, searchResult.getPosition());
+    }
+
+    @Test
+    void isMiddleElementInEvenArray() {
+        seq = new int[]{-1, key, 1, 2};
+        
+        keyPosition = seq.length / 2;
+        int keyPosition1 = keyPosition + 1;
+        searchResult = BinarySearch.search(key, seq);
+        assertEquals(true, searchResult.isFound());
+        int positionResult = searchResult.getPosition();
+        assertTrue((keyPosition == positionResult) || (keyPosition1 == positionResult);
     }
 
 }
