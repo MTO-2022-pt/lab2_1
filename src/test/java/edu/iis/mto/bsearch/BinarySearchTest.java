db47f0efd66228b49d27ff217ca2b4f53ab8ff8e
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-24 17:53:07 +0100

    testy_v3 added test to check if preconditions are met

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -11,6 +11,9 @@ class BinarySearchTest {
     private static final int[] seqBiggerOdd = {1, 3, 4, 5, 6, 7, 8};
     private static final int[] seqBiggerEven = {1, 3, 4, 5, 6, 7, 8, 9};
     private static final int[] seqEmpty = {};
+    private static final int[] seqNotSorted = {1, 6, 4, 7, 8, 2, 5};
+    private static final int[] seqSortedDescending = {9, 7, 6, 5, 4, 3, 2, 1};
+    private static final int[] seqWithDuplicates = {1, 2, 3, 3, 4, 5};
 
     @Test
     void elementIsInSeq() {
@@ -81,4 +84,23 @@ class BinarySearchTest {
         int keyToFind = -1;
         assertThrowsExactly(IllegalArgumentException.class, () -> BinarySearch.search(keyToFind, seqEmpty));
     }
+
+    @Test
+    void seqIsNotSortedAtAllShouldThrowsxception() {
+        int keyToFind = 4;
+        assertThrowsExactly(IllegalArgumentException.class, () -> BinarySearch.search(keyToFind, seqNotSorted));
+    }
+
+    @Test
+    void seqIsNotSortedAscendingShouldThrowsxception() {
+        int keyToFind = 4;
+        assertThrowsExactly(IllegalArgumentException.class, () -> BinarySearch.search(keyToFind, seqSortedDescending));
+    }
+
+    @Test
+    void seqConstainsRepeteadKeysShouldThrowsxception() {
+        int keyToFind = 4;
+        assertThrowsExactly(IllegalArgumentException.class, () -> BinarySearch.search(keyToFind, seqWithDuplicates));
+    }
+
 }
