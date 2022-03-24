1aba5712169d5d31f07112d43ac282efccff5151
patrycjastepora <patrycjastepora@gmail.com>
2022-03-24 21:37:21 +0100

    testy_v1 dodanie testow

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,15 +5,63 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class BinarySearchTest {
+    int key;
 
     @BeforeEach
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void isInSequence() {
+        int[] seqS = { 1 };
+        key = 0;
+        SearchResult result = BinarySearch.search(1, seqS);
+        assertTrue(result.isFound());
+        assertEquals(key, result.getPosition());
     }
 
+    @Test
+    void notInSequence() {
+        int[] seqS = { 1 };
+        key = -1;
+        SearchResult result = BinarySearch.search(2, seqS);
+        assertTrue(result.isFound());
+        assertEquals(key, result.getPosition());
+    }
+
+    @Test
+    void isFirstInSequence() {
+        int[] seqL = {1, 2, 3};
+        key = 0;
+        SearchResult result = BinarySearch.search(1, seqL);
+        assertTrue(result.isFound());
+        assertEquals(key, result.getPosition());
+    }
+
+    @Test
+    void isLastInSequence() {
+        int[] seqL = {4, 5, 6};
+        key = seqL.length - 1;
+        SearchResult result = BinarySearch.search(key, seqL);
+        assertTrue(result.isFound());
+        assertEquals(key, result.getPosition());
+    }
+
+    @Test
+    void isMiddleInSequence() {
+        int[] seqL = {7, 8, 9};
+        key = seqL.length/2;
+        SearchResult result = BinarySearch.search(key, seqL);
+        assertTrue(result.isFound());
+        assertEquals(key, result.getPosition());
+    }
+
+    @Test
+    void notLongInSequence() {
+        int[] seqL = {10, 11, 12};
+        key = seqL[0] - 1;
+        SearchResult result = BinarySearch.search(key, seqL);
+        assertFalse(result.isFound());
+        assertEquals(-1, result.getPosition());
+    }
 }
