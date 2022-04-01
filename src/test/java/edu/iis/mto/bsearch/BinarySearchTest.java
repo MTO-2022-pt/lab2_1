3be2779bcf4a5b3ff9173acc4996b5bee3057de9
Kunz.Dawid <228786@edu.p.lodz.pl>
2022-04-01 17:14:24 +0200

    testy_v1 dodanie testów do metody search

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,8 +12,52 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void test0()
+    {
+        int[] array = {1};
+        SearchResult ob = BinarySearch.search(1,array);
+        assertEquals(1,ob.getPosition());
+        assertTrue(ob.isFound());
+    }
+    @Test
+    void test1()
+    {
+        int[] array = {1};
+        SearchResult ob = BinarySearch.search(2,array);
+        assertEquals(-1,ob.getPosition());
+        assertFalse(ob.isFound());
+    }
+    @Test
+    void test2()
+    {
+        int[] array = {1, 2, 3, 9, 24, 26, 30, 45, 50};
+        SearchResult ob = BinarySearch.search(1,array);
+        assertEquals(1,ob.getPosition());
+        assertTrue(ob.isFound());
+    }
+    @Test
+    void test3()
+    {
+        int[] array = {1, 2, 3, 9, 24, 26, 30, 45, 50};
+        SearchResult ob = BinarySearch.search(50,array);
+        assertEquals(ob.getPosition(),array.length);
+        assertTrue(ob.isFound());
+    }
+    @Test
+    void test4()
+    {
+        int[] array = {1, 2, 3, 9, 24, 26, 30, 45, 50};
+        SearchResult ob = BinarySearch.search(24,array);
+        assertEquals(5,ob.getPosition());
+        assertTrue(ob.isFound());
+    }
+    @Test
+    void test5()
+    {
+        int[] array = {1, 2, 3, 9, 24, 26, 30, 45, 50};
+        SearchResult ob = BinarySearch.search(5,array);
+        assertEquals(-1,ob.getPosition());
+        assertFalse(ob.isFound());
     }
 
 }
