f70ae6a5e9cd73ea0b9b7dbf56c01f916c16ffd1
Tomasz Sztajkowski <222588@edu.p.lodz.pl>
2022-05-19 21:40:18 +0200

    testy_v1 size three tests

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -27,4 +27,36 @@ class BinarySearchTest {
         if(searchResult.getPosition() != -1) fail("SearchResult.getPosition() was not set to -1");
     }
 
+    @Test
+    void sizeThreeFirst() {
+        int[] sequence = {1, 2, 3};
+        SearchResult searchResult = BinarySearch.search(1, sequence);
+        if(!searchResult.isFound()) fail("SearchResult.isFound() was not set to true");
+        if(searchResult.getPosition() != 0) fail("SearchResult.getPosition() was not set to 0");
+    }
+
+    @Test
+    void sizeThreeLast() {
+        int[] sequence = {1, 2, 3};
+        SearchResult searchResult = BinarySearch.search(3, sequence);
+        if(!searchResult.isFound()) fail("SearchResult.isFound() was not set to true");
+        if(searchResult.getPosition() != 2) fail("SearchResult.getPosition() was not set to 2");
+    }
+
+    @Test
+    void sizeThreeMiddle() {
+        int[] sequence = {1, 2, 3};
+        SearchResult searchResult = BinarySearch.search(2, sequence);
+        if(!searchResult.isFound()) fail("SearchResult.isFound() was not set to true");
+        if(searchResult.getPosition() != 1) fail("SearchResult.getPosition() was not set to 1");
+    }
+
+
+    @Test
+    void sizeThreeNotInTable() {
+        int[] sequence = {1, 2, 3};
+        SearchResult searchResult = BinarySearch.search(4, sequence);
+        if(searchResult.isFound()) fail("SearchResult.isFound() was not set to false");
+        if(searchResult.getPosition() != -1) fail("SearchResult.getPosition() was not set to ;1");
+    }
 }
