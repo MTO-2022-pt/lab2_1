1238393b1bac0364fab29c03f662d2b89a3b5191
Tomasz Sztajkowski <222588@edu.p.lodz.pl>
2022-05-19 21:36:28 +0200

    testy_v1 size one tests

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,8 +12,19 @@ class BinarySearchTest {
     void setUp() throws Exception {}
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void sizeOneInTable() {
+        int[] sequence = {1};
+        SearchResult searchResult = BinarySearch.search(1, sequence);
+        if(!searchResult.isFound()) fail("SearchResult.isFound() was not set to true");
+        if(searchResult.getPosition() != 0) fail("SearchResult.getPosition() was not set to 0");
+    }
+
+    @Test
+    void sizeOneNotInTable() {
+        int[] sequence = {1};
+        SearchResult searchResult = BinarySearch.search(2, sequence);
+        if(searchResult.isFound()) fail("SearchResult.isFound() was not set to false");
+        if(searchResult.getPosition() != -1) fail("SearchResult.getPosition() was not set to -1");
     }
 
 }
